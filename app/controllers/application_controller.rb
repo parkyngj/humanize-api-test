1281f1c539fb0d05a7633a6116db742325741e95
spark <spark@MacBook-Air-2.local>
2016-11-05 21:52:22 -0700

    Comment out before filter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   # protect_from_forgery with: :exception
-  before_filter :cors_set_access_control_headers
+  # before_filter :cors_set_access_control_headers
 
   # def cors_set_access_control_headers
   #   headers['Access-Control-Allow-Origin'] = '*'
